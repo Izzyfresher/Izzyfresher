ff35e3f7292b29191ed12fe4d985368f91b17be8
Samuel <152026242+Samueladura@users.noreply.github.com>
2024-10-17 19:09:46 -0700

    import products from products.js

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,52 +1,5 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}, {
-    image: 'images/products/black-2-slot-toaster.jpg',
-    name: '2 Slot Toaster-Black',
-    rating: {
-        stars: 5,
-        count: 219
-    },
-    priceCents: 1899
-}, {
-    image: 'images/products/6-piece-white-dinner-plate-set.jpg',
-    name: '6 Piece White Dinner Plate Set',
-    rating: {
-        stars: 4,
-        count: 37
-    },
-    priceCents: 2067
-}, {
-    image: 'images/products/6-piece-non-stick-baking-set.webp',
-    name: '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set',
-    rating: {
-        stars: 4.5,
-        count: 175
-    },
-    priceCents: 34.99
-}];
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 let productsHTML = '';
 
@@ -96,13 +49,44 @@ products.forEach((products) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${products.id}">
             Add to Cart
           </button>
         </div>
     `;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener('click', () => {
+   const productId =  button.dataset.productId;
+
+   let matchItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchItem = item;
+      }
+    });
+
+    if (matchItem) {
+      matchItem.quantity +=1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+       });
+    }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+  });
+  });
